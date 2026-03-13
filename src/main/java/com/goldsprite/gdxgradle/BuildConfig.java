f3b7b6bfab766dc568ae99808a4927f17f0e6356
shikeik <2667097598@qq.com>
2026-03-14 06:19:38 +0800

    feat: bumpPatch 0.11.3

--- src/main/java/com/goldsprite/gdxgradle/BuildConfig.java
+++ src/main/java/com/goldsprite/gdxgradle/BuildConfig.java
@@ -14,6 +14,6 @@ public class BuildConfig {
 	public static final String DISPLAY_VERSION = "1.0.0#0";
 	
 	// 构建信息
-	public static final long BUILD_TIMESTAMP = 1773437307227L;
-	public static final String BUILD_TIME = "2026-03-14 05:28:27";
+	public static final long BUILD_TIMESTAMP = 1773440363887L;
+	public static final String BUILD_TIME = "2026-03-14 06:19:23";
 }
